b28cc3ee489751cf95a68397368597c5f55c3d2c
Stuart474 <royorjohn777@gmail.com>
2024-02-12 22:05:23 +0000

    Added Leader's Pennon models. Also added item displays. Also built a dll. :)

--- Source/ExamplePlugin/Items/LeadersPennon.cs
+++ Source/ExamplePlugin/Items/LeadersPennon.cs
@@ -23,12 +23,11 @@ namespace Faithful
             toolbox = _toolbox;
 
             // Create display settings
-            //CreateDisplaySettings();
+            CreateDisplaySettings();
 
             // Create Leader's Pennon item and buff
-            leadersPennonBuff = toolbox.buffs.AddBuff("LEADERS_PENNON", "texbufftemporalcube", Color.white, false);
-            leadersPennonItem = toolbox.items.AddItem("LEADERS_PENNON", [ItemTag.Utility, ItemTag.AIBlacklist, ItemTag.CannotCopy], "textemporalcubeicon", "temporalcubemesh", ItemTier.VoidTier1, _corruptToken: "ITEM_WARDONLEVEL_NAME");
-            //_displaySettings: displaySettings
+            leadersPennonBuff = toolbox.buffs.AddBuff("LEADERS_PENNON", "texbuffleaderarea", Color.white, false);
+            leadersPennonItem = toolbox.items.AddItem("LEADERS_PENNON", [ItemTag.Utility, ItemTag.AIBlacklist, ItemTag.CannotCopy], "texleaderspennonicon", "leaderspennonmesh", ItemTier.VoidTier1, _corruptToken: "ITEM_WARDONLEVEL_NAME", _displaySettings: displaySettings);
 
             // Add ally to ally behaviour
             toolbox.behaviour.AddAllyToAllyCallback(leadersPennonItem, AllyWithItemToAlly);
@@ -40,24 +39,22 @@ namespace Faithful
         private void CreateDisplaySettings()
         {
             // Create display settings
-            displaySettings = toolbox.utils.CreateItemDisplaySettings("meltingwarblerdisplaymesh");
+            displaySettings = toolbox.utils.CreateItemDisplaySettings("leaderspennondisplaymesh");
 
             // Add character display settings
-            displaySettings.AddCharacterDisplay("Commando", "Head", new Vector3(0F, 0.46155F, 0.00827F), new Vector3(15F, 0F, 0F), new Vector3(0.13F, 0.13F, 0.13F));
-            displaySettings.AddCharacterDisplay("Huntress", "Head", new Vector3(0F, 0.3575F, -0.025F), new Vector3(10F, 0F, 0F), new Vector3(0.11F, 0.11F, 0.11F));
-            displaySettings.AddCharacterDisplay("Bandit", "Head", new Vector3(0F, 0.275F, 0.02F), new Vector3(0F, 0F, 0F), new Vector3(0.1F, 0.1F, 0.1F));
-            displaySettings.AddCharacterDisplay("MUL-T", "Head", new Vector3(0F, 3F, 2.15F), new Vector3(305F, 180F, 0F), new Vector3(0.6F, 0.6F, 0.6F));
-            displaySettings.AddCharacterDisplay("Engineer", "Chest", new Vector3(0F, 0.7715F, 0.0975F), new Vector3(15F, 0F, 0F), new Vector3(0.11F, 0.11F, 0.11F));
-            displaySettings.AddCharacterDisplay("Turret", "Head", new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), new Vector3(1.0f, 1.0f, 1.0f));
-            displaySettings.AddCharacterDisplay("Artificer", "Head", new Vector3(0F, 0.25F, -0.0375F), new Vector3(25F, 0F, 0F), new Vector3(0.11F, 0.11F, 0.11F));
-            displaySettings.AddCharacterDisplay("Mercenary", "Head", new Vector3(0F, 0.325F, 0.07F), new Vector3(12.5F, 0F, 0F), new Vector3(0.125F, 0.125F, 0.125F));
-            displaySettings.AddCharacterDisplay("REX", "HandL", new Vector3(0F, 0.3F, 0.125F), new Vector3(280F, 180F, 0F), new Vector3(0.1F, 0.1F, 0.1F));
-            displaySettings.AddCharacterDisplay("Loader", "Head", new Vector3(0F, 0.3F, 0.0375F), new Vector3(5F, 0F, 0F), new Vector3(0.125F, 0.125F, 0.125F));
-            displaySettings.AddCharacterDisplay("Acrid", "Head", new Vector3(0F, 1.4325F, 2.15F), new Vector3(280F, 180F, 0F), new Vector3(1.35F, 1.35F, 1.35F));
-            displaySettings.AddCharacterDisplay("Captain", "Chest", new Vector3(0.3415F, 0.5145F, -0.045F), new Vector3(5F, 0F, 0F), new Vector3(0.11F, 0.11F, 0.11F));
-            displaySettings.AddCharacterDisplay("Railgunner", "GunStock", new Vector3(-0.001F, -0.015F, 0.09F), new Vector3(85.00005F, 180F, 180F), new Vector3(0.06F, 0.06F, 0.06F));
-            displaySettings.AddCharacterDisplay("Void Fiend", "Head", new Vector3(0F, 0.25F, -0.065F), new Vector3(345F, 0F, 0F), new Vector3(0.11F, 0.11F, 0.11F));
-            displaySettings.AddCharacterDisplay("Scavenger", "Chest", new Vector3(0F, 7.25F, 1.4F), new Vector3(350F, 180F, 0F), new Vector3(2.4F, 2.4F, 2.4F));
+            displaySettings.AddCharacterDisplay("Commando", "Chest", new Vector3(0F, 0.3775F, -0.21275F), new Vector3(1F, 180F, 0F), new Vector3(0.15F, 0.15F, 0.15F));
+            displaySettings.AddCharacterDisplay("Huntress", "Chest", new Vector3(0F, 0.125F, -0.1275F), new Vector3(5F, 180F, 0F), new Vector3(0.075F, 0.075F, 0.075F));
+            displaySettings.AddCharacterDisplay("Bandit", "MainWeapon", new Vector3(-0.06125F, 0.5415F, -0.0255F), new Vector3(1F, 172.75F, 0F), new Vector3(0.09F, 0.09F, 0.09F));
+            displaySettings.AddCharacterDisplay("MUL-T", "Chest", new Vector3(0F, 4F, -1.725F), new Vector3(0F, 180F, 0F), new Vector3(1F, 0.8F, 1F));
+            displaySettings.AddCharacterDisplay("Engineer", "Chest", new Vector3(0.003F, 0.47825F, -0.2905F), new Vector3(0F, 180F, 0F), new Vector3(0.25F, 0.25F, 0.25F));
+            displaySettings.AddCharacterDisplay("Artificer", "Chest", new Vector3(0.1165F, 0.448F, -0.275F), new Vector3(0F, 180F, 0F), new Vector3(0.11F, 0.11F, 0.11F));
+            displaySettings.AddCharacterDisplay("Mercenary", "UpperArmR", new Vector3(-0.16175F, 0.005F, -0.03925F), new Vector3(3.50988F, 257.5009F, 173.5443F), new Vector3(0.125F, 0.125F, 0.125F));
+            displaySettings.AddCharacterDisplay("REX", "PlatformBase", new Vector3(0F, 0.4F, -0.3725F), new Vector3(0F, 180F, 0F), new Vector3(0.12F, 0.12F, 0.12F));
+            displaySettings.AddCharacterDisplay("Loader", "MechBase", new Vector3(0.00098F, 0.06F, -0.1515F), new Vector3(359F, 180F, 0F), new Vector3(0.125F, 0.125F, 0.125F));
+            displaySettings.AddCharacterDisplay("Acrid", "Head", new Vector3(0F, 0.94F, 1.51255F), new Vector3(348.9F, 0F, 180F), new Vector3(1.35F, 1.35F, 1.35F));
+            displaySettings.AddCharacterDisplay("Captain", "Chest", new Vector3(0F, 0.32F, -0.235F), new Vector3(3.5F, 180F, 0F), new Vector3(0.2F, 0.2F, 0.2F));
+            displaySettings.AddCharacterDisplay("Railgunner", "Backpack", new Vector3(0.055F, -0.035F, -0.11F), new Vector3(0F, 178F, 14F), new Vector3(0.125F, 0.125F, 0.125F));
+            displaySettings.AddCharacterDisplay("Void Fiend", "Chest", new Vector3(-0.005F, 0.345F, -0.205F), new Vector3(5F, 180F, 0F), new Vector3(0.125F, 0.125F, 0.125F));
         }
 
         void LeadersPennonStatsMod(int _count, RecalculateStatsAPI.StatHookEventArgs _stats)
