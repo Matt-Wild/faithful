84bbc28015ad4aa91e03831fcd6282091cd3bafe
Stuart474 <royorjohn777@gmail.com>
2025-06-03 21:20:23 +0100

    Generated a soundbank for testing that includes the intended sound effect for the Targeting Matrix. :)

---
+++ BundleProject/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,51 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID TARGETINGMATRIXLOCKON = 3182601471U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID FAITHFUL_CHAR_CASTER = 4281578430U;
+        static const AkUniqueID FAITHFUL_CHAR_GYRO = 463116895U;
+        static const AkUniqueID FAITHFUL_CHAR_HUNTSMAN = 1703840500U;
+        static const AkUniqueID FAITHFUL_CHAR_OPERATOR = 3952637814U;
+        static const AkUniqueID FAITHFUL_CHAR_PROXY = 2921305306U;
+        static const AkUniqueID FAITHFUL_CHAR_SALVAGER = 2165282721U;
+        static const AkUniqueID FAITHFUL_CHAR_SURVEYOR = 3550526995U;
+        static const AkUniqueID FAITHFUL_CHAR_TECHNICIAN = 3251809208U;
+        static const AkUniqueID FAITHFUL_CHAR_VOIDAGENT = 2287558135U;
+        static const AkUniqueID FAITHFUL_ITEM_GLOBAL = 388688204U;
+        static const AkUniqueID FAITHFUL_MOB_GLOBAL = 1066124047U;
+        static const AkUniqueID FAITHFUL_OBJ_GLOBAL = 2620265520U;
+        static const AkUniqueID FAITHFUL_OTHER_GLOBAL = 2608188423U;
+        static const AkUniqueID FAITHFUL_ZONE_GLOBAL = 1082257441U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
